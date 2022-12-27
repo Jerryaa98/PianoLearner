ac9ec9fd17e1594ff0c1f3994b3b6fe740e3440c
Jerryaa98 <jerryabuayob@gmail.com>
2022-12-27 16:48:48 +0200

    add scene with piano

---
+++ Assets/Scripts/AutoGuideSong.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoGuideSong : MonoBehaviour
+{
+
+    List<(string, string, double, double)> keysToPlay;
+
+    public Material ColorMaterial;
+    private Material originalMaterial;
+    private bool isLit = false;
+    private (string, string, double, double) litKey;
+
+    private float startTime = 0;
+
+    public string SongName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keysToPlay = new List<(string, string, double, double)>();
+
+        var asset = Resources.Load<TextAsset>($"Songs/{SongName}");
+        string[] lines = asset.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach(var line in lines)
+        {
+            string[] split = line.Trim().Split(',');
+            keysToPlay.Add((split[0], split[1], double.Parse(split[2]), double.Parse(split[3])));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(keysToPlay != null)
+        {
+            PlaySong();
+        }
+    }
+
+    void PlaySong()
+    {
+        foreach (var currentKey in keysToPlay)
+        {
+            if (!isLit && Time.time >= currentKey.Item3 + startTime && Time.time <= currentKey.Item4 + startTime)
+            {
+                originalMaterial = GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material;
+                GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material = ColorMaterial;
+                isLit = true;
+                litKey = currentKey;
+            }
+            if (isLit && currentKey == litKey && Time.time > litKey.Item4 + startTime)
+            {
+                GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material = originalMaterial;
+                isLit = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ClickGuideSong.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickGuideSong : MonoBehaviour
+{
+    List<(string, string, double, double)> keysToPlay;
+
+    public Material ColorMaterial;
+    private Material originalMaterial;
+    private int litKey = 0;
+
+    public string SongName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keysToPlay = new List<(string, string, double, double)>();
+
+        var asset = Resources.Load<TextAsset>($"Songs/{SongName}");
+        string[] lines = asset.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            string[] split = line.Trim().Split(',');
+            keysToPlay.Add((split[0], split[1], double.Parse(split[2]), double.Parse(split[3])));
+        }
+        litKey = 0;
+        var currentKey = keysToPlay[litKey];
+        originalMaterial = GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material;
+        GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material = ColorMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void  ClickKey(GameObject obj)
+    {
+        if(litKey >= keysToPlay.Count)
+        {
+            return;
+        }
+        var currentKey = keysToPlay[litKey];
+        // Debug.Log($"{currentKey.Item1} , {currentKey.Item2}");
+        if(string.Equals(currentKey.Item2, obj.name, StringComparison.InvariantCultureIgnoreCase) &&
+            string.Equals(currentKey.Item1, obj.transform.parent.name, StringComparison.InvariantCultureIgnoreCase))
+        {
+            GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material = originalMaterial;
+
+            litKey++;
+            if (litKey >= keysToPlay.Count)
+            {
+                return;
+            }
+            currentKey = keysToPlay[litKey];
+            originalMaterial = GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material;
+            GameObject.Find($"{currentKey.Item1}/{currentKey.Item2}/MovingKey").GetComponent<MeshRenderer>().material = ColorMaterial;
+        }
+    }
+}
